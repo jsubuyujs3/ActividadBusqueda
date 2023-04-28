71562135e76215798f6719a70ba4f242f5a852ef
Jose Daniel Subuyuj Subuyuj <jsubuyujs3@miumg.edu.gt>
2023-04-27 23:04:54 -0600

    Creacion de la clase nodo

---
+++ src/actividadbusqueda/Nodo.java
@@ -0,0 +1,44 @@
+
+package actividadbusqueda;
+
+
+public class Nodo {
+    
+    private short arriba;
+    private short abajo;
+    private short izquierda;
+    private short derecha;
+
+    public short getArriba() {
+        return arriba;
+    }
+
+    public void setArriba(short arriba) {
+        this.arriba = arriba;
+    }
+
+    public short getAbajo() {
+        return abajo;
+    }
+
+    public void setAbajo(short abajo) {
+        this.abajo = abajo;
+    }
+
+    public short getIzquierda() {
+        return izquierda;
+    }
+
+    public void setIzquierda(short izquierda) {
+        this.izquierda = izquierda;
+    }
+
+    public short getDerecha() {
+        return derecha;
+    }
+
+    public void setDerecha(short derecha) {
+        this.derecha = derecha;
+    }
+
+}
